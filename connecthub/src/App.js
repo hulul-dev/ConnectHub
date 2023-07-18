fc085798df6b09565a1fbd33b067a0a9dc3cc010
Mohamed Haseeb <mohamedhaseeb.developer@gmail.com>
2023-07-18 22:13:13 +0530

    Login

---
+++ connecthub/src/App.js
@@ -0,0 +1,26 @@
+import './App.css'
+import Login from './components/Login'
+import Header from './components/Header'
+import Home from './components/Home'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route exact path= "/">
+            <Login />
+          </Route>
+          <Route path= "/home">
+            <Header />
+            <Home />
+          </Route>            
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+export default App 
